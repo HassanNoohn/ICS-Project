865e10b6737b6d5f488c8658b086e0ebe194d346
HassanNoohn <91547755+HassanNoohn@users.noreply.github.com>
2022-01-17 11:32:27 -0500

    Add files via upload

--- covidCraze.py
+++ covidCraze.py
@@ -39,8 +39,15 @@ keepGoing = True 	    #<-- a 'flag' variable for the game loop condition
 
 # Set up the font and the size 
 bigfont = pygame.font.SysFont("comicsansms", 42)
+font_score = pygame.font.SysFont("Bauhaus 93", 30)
+
+#define colours
+white = (255, 255, 255)
 
 #sub programs
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
 def setBackground(file):
   image=pygame.image.load(file).convert()
   image=pygame.transform.scale(image, (800, 449))  
@@ -49,12 +56,254 @@ def button(file):
   button=pygame.image.load(file).convert_alpha()
   button=pygame.transform.scale(button, (100, 40))
   return button
+
 def quizOption(file):
   option=pygame.image.load(file).convert_alpha()
   option=pygame.transform.scale(option,(129,109))
   return option
+#--------game stuff----------
+screen_width = 800
+screen_height = 449
 
-#test=pygame.display.get_driver()
+#game variables
+tile_size = 30
+game_over = 0
+score = 0
+
+#images for game
+sun_img = pygame.image.load('img/sun.png')
+bg_img = pygame.image.load('img/sky.png')
+
+#functions below all by Coding With Russ on YouTube
+
+class Player():
+    def __init__(self, x, y):
+         self.reset(x, y)
+
+
+    def update(self, game_over):
+        dx = 0
+        dy = 0
+        walk_cooldown = 5
+
+        if game_over == 0:
+            #get keypresses
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE] and self.jumped == False:
+                self.vel_y = -15
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+            if key[pygame.K_LEFT]:
+                dx -= 5
+                self.counter += 1
+                self.direction = -1
+            if key[pygame.K_RIGHT]:
+                dx += 5
+                self.counter += 1
+                self.direction = 1
+            if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+                self.counter = 0
+                self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+
+
+            #handle animation
+            if self.counter > walk_cooldown:
+                self.counter = 0    
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+
+
+            #add gravity
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
+
+            #check for collision
+            for tile in world.tile_list:
+                #check for collision in x direction
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                #check for collision in y direction
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    #check if below the ground i.e. jumping
+                    if self.vel_y < 0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    #check if above the ground i.e. falling
+                    elif self.vel_y >= 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
+
+            #check for collision with coins            
+            if pygame.sprite.spritecollide(player, coin_group, True):
+                score += 1
+
+            #check for collision with enemies
+            if pygame.sprite.spritecollide(self, blob_group, False):
+                game_over = -1
+            #check for collision with exit
+            if pygame.sprite.spritecollide(self, exit_group, False):
+                game_over = 1
+
+            #update player coordinates
+            self.rect.x += dx
+            self.rect.y += dy
+
+
+        elif game_over == -1:
+            self.image = self.dead_image
+            if self.rect.y > 200:
+                self.rect.y -= 5
+
+
+                
+                
+
+        #draw player onto screen
+        screen.blit(self.image, self.rect)
+        pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
+
+        return game_over
+
+    def reset(self, x, y):
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 5):
+            img_right = pygame.image.load(f'img/guy{num}.png')
+            img_right = pygame.transform.scale(img_right, (40, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        self.dead_image = pygame.image.load('img/ghost.png')
+        self.image = self.images_right[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.direction = 0
+        self.in_air = True
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        #load images
+        dirt_img = pygame.image.load('img/dirt.png')
+        grass_img = pygame.image.load('img/grass.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+                    blob_group.add(blob)
+                if tile == 4:
+                    coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+                    coin_group.add(coin)
+                if tile == 8:
+                    exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+                    exit_group.add(exit)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/blob.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/coin.png')
+        self.image = pygame.transform.scale(img, (int(tile_size * 1.5), int(tile_size * 1.5)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+    
+            
+class Exit(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/exit.png')
+        self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 1], 
+[1, 0, 3, 0, 0, 2, 2, 2, 0, 0, 0, 0, 2, 2, 0, 0, 0, 2, 2, 0, 0, 0, 2, 2, 2, 2, 1], 
+[1, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 2, 2, 2, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 1],
+[1, 0, 0, 0, 0, 2, 2, 2, 0, 0, 3, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+[1, 0, 0, 0, 2, 1, 1, 1, 4, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+player = Player(100, screen_height - 130)
+blob_group = pygame.sprite.Group()
+coin_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
+
+world = World(world_data)
+
 state="title"
 next=False
 btnNew=button("btnPlay.png")
@@ -90,16 +339,42 @@ try:
         be=screen.blit(btnExit,(600,320))    #be- rectangle around buttonExit
 
     elif state=="game":  
-            # ---------------code for the game-------------------               
-      screen.blit(welcome,(20,50))   # print text welcome
-      bb=screen.blit(btnBack,(400,380))   #bb- rectangle around button btnBack
+      screen.blit(bg_img, (0, 0))
+      screen.blit(sun_img, (100, 100))
+
+      world.draw()
+
+      if game_over == 0:
+        blob_group.update()
+
+        if pygame.sprite.spritecollide(player, coin_group, True):
+          score += 1
+        draw_text("X " + str(score), font_score, white, tile_size - 10, 10)
+    
+      blob_group.draw(screen)
+      coin_group.draw(screen)
+      exit_group.draw(screen)
+
+      game_over = player.update(game_over)
+      
+      #if play has died
+      if game_over == -1:
+            player.reset(100, screen_height - 130)
+            game_over = 0   
+            score = 0
+      #if player has completed the level
+      if game_over == 1:
+            #reset game and go to next level
+            state="menu"
 
     elif state=="quiz":
             # ---------------code for the quiz-------------------
-      question=int(0)
-      correctAnswers=[]
-      userAnswers=[]
+      question=0
+      correctAnswers=0
+      correct=bool()
+
       setBackground("quiz.png")
+
       btnNext=button("btnNext.png")
       bnq=screen.blit(btnNext,(650,380))   #bnq- rectangle around button btnNext(questions)
       bb=screen.blit(btnBack,(50,380))   #bb- rectangle around button btnBack
@@ -120,17 +395,19 @@ try:
 
       option4=quizOption("q"+str(question)+"o4.png")
       o4=screen.blit(option4,(635,200))
-
-      if question<5:
-        bnq=screen.blit(btnNext,(650,380))   
+      if correct:
+        setBackground("correct.png")
+        bnq=screen.blit(btnNext,(650,380)) 
       else:
-        bb=screen.blit(btnBack,(50,380))
+        setBackground("incorrect.png")
+        bnq=screen.blit(btnNext,(650,380)) 
 
     elif state=="result":
             # ---------------code for the quiz results-------------------
-      screen.blit(result,(20,70))  # print text for results
-      bb=screen.blit(btnBack,(400,380))    #bb- rectangle around button btnBack
-            
+      setBackground("result.png")
+      bb=screen.blit(btnBack,(400,380))   #bb- rectangle around button btnBack
+
+
     elif state=="lesson":
             # ---------------code for the lesson 
       lessonNumber=0            
@@ -160,33 +437,63 @@ try:
       elif ev.type == MOUSEBUTTONDOWN:
         pos=pygame.mouse.get_pos()
         if bm.collidepoint(pos):
-          state="menu"               
-        elif bp.collidepoint(pos):
-          state="game"
-        elif bq.collidepoint(pos):
-          state="quiz"
-        elif br.collidepoint(pos):
-          state="result"   
-        elif bl.collidepoint(pos):
-          state="lesson"  
+          state="menu" 
 
-        elif state=="quiz" or state=="questions":
+        elif state=="menu":              
+          if bp.collidepoint(pos):
+            state="game"
+          elif bq.collidepoint(pos):
+            state="quiz"
+          elif br.collidepoint(pos):
+            state="result"   
+          elif bl.collidepoint(pos):
+            state="lesson"  
+          elif be.collidepoint(pos):
+            keepGoing = False
+
+        elif state=="results":
+          
+          if bb.collidepoint(pos):
+            state="menu"  
+
+        elif state=="quiz":
           if bnq.collidepoint(pos):
             state="questions"
             question+=1
           elif bb.collidepoint(pos):
-            state="menu"
+            state="menu"  
+
+        elif state=="questions":
+            if bnq.collidepoint(pos):
+              question+=1
+
+            #correct answers
+            if o1.collidepoint(pos) and question!=1 or question!=5:
+              correctAnswers+=1
+              correct=True
+            elif o3.collidepoint(pos) and question==5:
+              correctAnswers+=1
+              correct=True
+            elif o4.collidepoint(pos) and question==1:
+              correctAnswers+=1
+              correct=True
+
+            #wrong answers
+            elif (o1.collidepoint(pos) and (question==1 or question==5)) or o2.collidepoint(pos) or (o3.collidepoint(pos) and question!=5) or (o4.collidepoint(pos) and question!=1):
+              correct=False
+
+            if question==5:
+              state="menu"
+
+        #elif bb.collidepoint(pos):
+          #state="menu"
 
         elif state=="lesson" or state=="lessons":
-            if bnl.collidepoint(pos):
-              state="lessons" 
-              lessonNumber+=1    
-            elif bb.collidepoint(pos):
-              state="menu"     
-        elif bb.collidepoint(pos):
-            state="menu"
-        elif be.collidepoint(pos):
-            keepGoing = False
+          if bnl.collidepoint(pos):
+            state="lessons" 
+            lessonNumber+=1    
+          elif bb.collidepoint(pos):
+            state="menu"  
 
 finally:
   pygame.quit()  # Keep this IDLE friendly 
